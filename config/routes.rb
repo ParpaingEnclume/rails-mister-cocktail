df6e282f8f004eae94739c594979ae2988f850f2
raphael birotheau <raphael.birotheau@gmail.com>
2019-08-16 12:10:40 +0200

    v4

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #   get 'cocktails', to: 'cocktails#index'
   #   get 'cocktail', to: 'cocktails#show'
-  root to: "cocktails#index"
+    root to: "cocktails#index"
   resources :cocktails, only: [:new, :create, :show, :index] do
     resources :doses, only: [:new, :create, :show]
   end
+  resources :doses, only: [:destroy]
 end
