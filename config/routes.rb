2f7f3800ad4a1cc9b8bddf2481b8fedcdb58f712
raphael birotheau <raphael.birotheau@gmail.com>
2019-08-16 11:32:18 +0200

    v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #   get 'cocktails', to: 'cocktails#index'
+  #   get 'cocktail', to: 'cocktails#show'
+  resources :cocktails, only: [:new, :create, :show, :index] do
+    resources :doses, only: [:new, :create, :show]
+  end
 end
